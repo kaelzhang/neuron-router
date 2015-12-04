c0c77cd8b5eeb7091ba9f1642a6ce980c1c93b59
Kael <i@kael.me>
2015-12-04 11:32:11 +0800

    main, tests

--- index.js
+++ index.js
@@ -9,26 +9,8 @@ var fs = require('fs');
 //   - routers: `Array`
 //   - by_pass: `String` 
 exports.route = function (path, config, callback) {
-  if (Object(config) !== config) {
-    return null;
-  }
-
-  var routers = config.routers;
-
-  if (!path || !routers || !routers.length) {
-    return null;
-  }
-
-  var found;
-  routers.some(function (router) {
-    if (path.indexOf(router.location) === 0) {
-      found = router;
-      return true;
-    }
-  });
-
-  if (!found) {
-    return process.nextTick(function () {
+  function none () {
+    process.nextTick(function () {
       if (!config.by_pass) {
         return callback(null, null);
       }
@@ -38,22 +20,59 @@ exports.route = function (path, config, callback) {
     });
   }
 
-  var pathname = parsed.pathname.slice(router.location.length);
+  if (Object(config) !== config) {
+    return none();
+  }
+
+  var routers = config.routers;
+
+  if (!path || !routers || !routers.length) {
+    return none();
+  }
+
+  var found;
+  var pathname;
+  routers.some(function (router) {
+    if (path.indexOf(router.location) !== 0) {
+      return;
+    }
+
+    var p = path.slice(router.location.length);
+
+    if (/^\//.test(p)) {
+      found = router;
+      pathname = exports._remove_leading_slash(p);
+      return true;
+    }
+  });
+
+  if (!found) {
+    return none();
+  }
+
   // TODO: check if req.url has queries
-  var filename = node_path.join(router.root, pathname);
+  var filename = node_path.join(found.root, pathname);
 
   fs.exists(filename, function (exists) {
     if (exists) {
-      return callback(filename);
+      return callback(filename, null);
     }
 
-    callback(null, router.by_pass 
-      ? node_path.join(router.by_pass, pathname)
-      : null);
+    callback(
+      null, 
+      router.by_pass 
+        ? node_path.join(router.by_pass, pathname)
+        : null
+    );
   });
 };
 
 
 exports._make_sure_trailing_slash = function (str) {
   return str.replace(/\/*$/, '/');
+};
+
+
+exports._remove_leading_slash = function (str) {
+  return str.replace(/^\/+/, '');
 };
--- test/neuron-router.js
+++ test/neuron-router.js
@@ -3,10 +3,23 @@
 var expect = require('chai').expect;
 var neuron_router = require('../');
 var run = require('run-mocha-cases');
+var node_path = require('path');
+
+var _root = node_path.join(__dirname, 'fixtures')
+
+function root (path) {
+  return node_path.join(_root, path);
+}
 
 var routers = [
   {
-    location: '/'
+    location: '/a',
+    root: root('a')
+  },
+
+  {
+    location: '/b/',
+    root: root('b')
   }
 ];
 
@@ -14,10 +27,17 @@ run
   .description('router.route()')
   .runner(function (path) {
     var done = this.async();
-    neuron_router.route(path, routers, done);
+    neuron_router.route(path, {
+      routers: routers
+    }, done);
   })
   .start([
     {
-      d: ''
+      d: 'normal',
+      a: '/a/a.js',
+      e: [
+        root('a/a.js'),
+        null
+      ]
     }
   ]);
